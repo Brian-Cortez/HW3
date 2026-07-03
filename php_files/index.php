9d2ed9f556c98e0e41eab2370a8f71d544ad39f2
Brian-Cortez <briancortezreyescollage@gmail.com>
2026-07-03 15:00:08 -0400

    added all the selection options for the customer

--- php_files/index.php
+++ php_files/index.php
@@ -24,16 +24,36 @@
                 <label for="cn">Customer Name: </label>
                 <input type="text" name="Customer Name" id="cn"  size="30"/>
 
-                <label for="dn">Drinks</label>
+
+                <label for="desrt">Dessert: </label>
+                <select id="desrt" name="Dessert">
+                    <option>Select One...</option>
+                    <option value="Cheesecake" <?= dessertSelected("Cheesecake"); ?>>Cheesecake</option>
+                    <option value="Chocolate Cake" <?= dessertSelected("Chocolate Cake"); ?>>Chocolate Cake</option>
+                    <option value="Tiramisu" <?= dessertSelected("Tiramisu"); ?>>Tiramisu</option>
+                    <option value="Carrot Cake" <?= dessertSelected("Carrot Cake"); ?>>Carrot Cake</option>
+                    <option value="Vanilla Milkshake" <?= dessertSelected("Vanilla Milkshake"); ?>>Vanilla Milkshake</option>
+
+                </select>
+
+                <label for="dn">Drinks: </label>
                 <select id="dn" name="drinks">
                     <option>Select One...</option>
-                    <option>Coffee</option>
-                    <option>Chocolate Milk</option>
-                    <option>Boba</option>
-                    <option>Tea</option>
-                    <option>Milk</option>
-                    <option>Juice</option>
-                    <option>Root Beer</option>
+                    <option value="Coffee" <?= drinkSelected("Coffee"); ?>>Coffee</option>
+                    <option value="Chocolate Milk" <?= drinkSelected("Chocolate Milk"); ?>>Chocolate Milk</option>
+                    <option value="Boba" <?= drinkSelected("Boba"); ?>>Boba</option>
+                    <option value="Tea" <?= drinkSelected("Tea"); ?>>Tea</option>
+                    <option value="Milk" <?= drinkSelected("Milk"); ?>>Milk</option>
+                    <option value="Juice" <?= drinkSelected("Juice"); ?>>Juice</option>
+                    <option value="Root Beer" <?= drinkSelected("Root Beer"); ?>>Root Beer</option>
+                </select>
+
+                <label for="ds">Drink Size: </label>
+                <select id="ds" name="drinkSize">
+                    <option>Select One...</option>
+                    <option value="Small" <?= drinkSizeSelected("Small")?>>Small</option>
+                    <option value="Medium" <?= drinkSizeSelected("Medium")?>>Medium</option>
+                    <option value="Large" <?= drinkSizeSelected("Large")?>>Large</option>
                 </select>
 
             </div>
@@ -42,7 +62,7 @@
 
         </form>
     </section>
-    
+
     </main>
 </body>
 </html>
